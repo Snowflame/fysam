b3c7592cf18afd9c1d87cc61f77aef3c18f00d90
javier.pena <javier.pena@kaufda.de>
2015-07-01 20:57:10 +0200

    spotify saves it

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -25,28 +25,24 @@ module.exports = {
 	  	var SpotifyWebApi = require('spotify-web-api-node');
 
 		// credentials are optional
-		var spotifyApi = new SpotifyWebApi({
-		 	clientId : 'fcecfc72172e4cd267473117a17cbd4d',
-	  		clientSecret : 'a6338157c9bb5ac9c71924cb2940e1a7',
-		  	redirectUri : 'http://www.example.com/callback'
-		});
+		var spotifyApi = new SpotifyWebApi();
 		spotifyApi.getArtistAlbums(network.artistId)
 			.then(function(data) {
 				if(network.postCount < data.body.total) {
 					var items = data.body.items;
 					for (i in items) {
 						var item = items[i];
-						console.log(item);
 						var post = Post.find({id:item.id}).exec(function (err, post){
 							if(err)
 								res.send(err);
 							else if (post.length == 0) {
 								Post.create({
 									date: new Date(),
-									url: item.external_urls,
-									owner: network
-								}).exec(function createCB(err, created){
-									console.log('Created post');
+									url: item.external_urls.spotify,
+									owner: network.id
+								}).exec(function (err, created){
+									console.log(created);
+									console.log(err);
 								});
 								self.hi(req, res, i);
 							}
