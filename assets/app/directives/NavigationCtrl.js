36c4f84f6297b53d9c6ff2f8c7f62903a0b16472
Borkowski <mborkows@XIN853763.asv.local>
2015-07-02 00:33:34 +0200

    chnage chlass on location

---
+++ assets/app/directives/NavigationCtrl.js
@@ -0,0 +1,5 @@
+app.controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.isCurrentPath = function (path) {
+      return $location.path() == path;
+    };
+  }]);
